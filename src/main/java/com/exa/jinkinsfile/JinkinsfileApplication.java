c7c63675008991e7938c2686e001d22714879a36
hutao <18392973501@163.com>
2019-08-06 17:39:08 +0800

    test

---
+++ src/main/java/com/exa/jinkinsfile/JinkinsfileApplication.java
@@ -0,0 +1,13 @@
+package com.exa.jinkinsfile;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JinkinsfileApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JinkinsfileApplication.class, args);
+    }
+
+}
